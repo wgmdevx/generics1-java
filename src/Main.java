27b451f1570a67cdb946fcd4e67c40dac586a7ae
wgmdevx <wgmdevx@gmail.com>
2025-02-28 13:17:31 -0300

    Integer print service

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import service.PrintService;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintService ps = new PrintService();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i=0; i < n; i++) {
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First " + ps.first());
+
+
+        sc.close();
+    }
+}
